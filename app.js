15a5bf20a8c1c39ff3ab04c88f053c6140865e5a
Ryan Rhodes <rrhodes.webdev@gmail.com>
2018-01-05 21:35:19 -0500

    Trail Data Pulling
    Connect Geocode data with Trail data, pulling correct data from REI Hiking project API

--- app.js
+++ app.js
@@ -8,17 +8,15 @@ function userSubmitData() {
   $('.js-search-form').on('click', '.js-submit-btn', function(event) {
     event.preventDefault();
 
-    console.log("user clicked");
-
     let userValue = $('#input-field').val();
 
-    console.log(userValue);
-
-    fetchGoogleGeoData(userValue, renderResultList);
+    fetchGoogleGeoData(userValue, fetchTrailData);
 
     userValue = $('#input-field').val("");
   })
 }
+
+console.log("userSubmitData executed");
 
 //API request to Google Maps
 
@@ -37,7 +35,23 @@ $.getJSON(GEOCODE_API, query, callback);
 
 //API request to REI Hiking Project
 
-function fetchTrailData() {
+function fetchTrailData(data, query, callback) {
+
+  let lat = data.results[0].geometry.location.lat;
+  let lon = data.results[0].geometry.location.lng;
+
+  console.log(lat);
+  console.log(lon);
+
+  const newQuery = {
+    key: "200202949-be5202662091a9dc38356c0c802cd058",
+    lat: lat,
+    lon: lon
+  }
+
+  console.log(newQuery);
+
+  $.getJSON(GETTRAIL_API, newQuery, resultList)
 
 }
 
@@ -49,13 +63,23 @@ function fetchWeatherData() {
 
 //Display a map and list of trails around the location value
 
-function renderResultList(data) {
+function renderResults(item) {
+  return `
+    <div class="individual-trail">
+      <h2>item.name</h2>
+    </div>
+  `
+}
 
-let lat = data.results[0].geometry.location.lat;
-let lon = data.results[0].geometry.location.lng;
+function resultList(data) {
 
-console.log(lat);
-console.log(lon);
+  console.log(data);
+
+  let trailInfo = data.map(item => {
+    renderResults(item);
+  });
+
+  $('.js-search-results').html(trailInfo);
 
 }
 
